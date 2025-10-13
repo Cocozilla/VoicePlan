7aa65a1701bbddfca1840ed5fe8ff44e824cc370
Numair Ahmed <numairx120@gmail.com>
2025-10-13 21:00:42 +0000

    Fix local auth and prep for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
     output: 'standalone',
-    // Only add the env block if it's a production build
-    ...(isProduction && {
-        env: {
-            NEXT_PUBLIC_FIREBASE_WEBAPP_CONFIG: process.env.FIREBASE_WEBAPP_CONFIG,
-        }
-    })
 };
 
 export default nextConfig;
